b97b95754a598ea97322b42589e62bd68fff94d7
sebastian <smgonalez@gmail.com>
2025-09-07 20:50:45 +0200

    se agg anotaciones, y la funcion de agregar elementos a la lista

--- ProyectoTienda/src/main/java/com/proyectotienda/ProyectoTienda/controller/ClienteController.java
+++ ProyectoTienda/src/main/java/com/proyectotienda/ProyectoTienda/controller/ClienteController.java
@@ -1,11 +1,44 @@
 package com.proyectotienda.ProyectoTienda.controller;
 
+
+import com.proyectotienda.ProyectoTienda.model.Cliente;
+import com.proyectotienda.ProyectoTienda.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+//anotaciones
+
+@Controller
+
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    //crud ruta logica
+    //meotdos controlados e inyeccion automatica de las dependencias
 
+    @Autowired
+    private ClienteRepository clienteRepository;
 
+    //mostrat lista de clientes
 
+    //anotacion
+    @GetMapping
+    public String listarClientes(Model model){
+        //mostrar todos los clientes de la base de datos
 
+        List<Cliente> clientes=clienteRepository.findAll();
+
+        //agregar elementos a la lista
+
+        model.addAttribute("clientes",clientes);
+
+        return "clientes";
+
+    }
 
 }
